2b965124244b2a90f77e3569fa20810030716148
yoyolalala <xuyaolalala@163.com>
2017-11-03 21:08:21 +0800

    init the pro

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # stm32_RTC_secs
-config RTC clock to record system running time
+## config RTC clock to record system running time
+- get the number of seconds in system operation
+- change the resetDateFlag to choose whether to reset time when pressing reset button
+
+## TODO
+- get millis in system operation
---
+++ USER/main.c
@@ -0,0 +1,21 @@
+#include "sys.h"
+#include "delay.h"
+#include "usart.h"
+#include "rtc.h"
+#include <stdbool.h>
+void setup()
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);//设置系统中断优先级分组4
+	delay_init(168);    //初始化延时函数
+	uart_init(38400);
+	My_RTC_Init();
+}
+
+int main(void)
+{ 
+	setup();
+	while(1)
+	{
+		printf("secs = %lu\r\n",secs());
+	}	
+}
